70b4f437385a46a3f3c73fb5804ba22bfa145181
Sebastian <se.lorcav@gmail.com>
2019-02-10 23:20:24 -0300

    primer commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-app.js
+$(function(){
+	var url = 'https://mindicador.cl/api';
+	$(".btn-info").on("click", function(){
+			var valor = $(".valorpeso").val();
+			var valordolar = $(".dolar").html();
+			var resultadodolar = valor/valordolar;
+			console.log(valor, valordolar)
+
+		})
+
+	$(function(){
+		$.ajax({
+			url: url,
+			method:'GET',
+		}).then(function(data){
+			console.log(data);
+			$(".dolar").html("CLP " + (data.dolar.valor).toFixed(0));
+			$(".Euro").html("CLP " + (data.euro.valor).toFixed(0));
+			$(".uf").html("CLP " + (data.uf.valor).toFixed(0));
+			$(".utm").html("CLP " + (data.utm.valor).toFixed(0));
+		});
+	})
+});
